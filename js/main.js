5688af0e749cfa031412044b0fcdc34b024fc573
mingwiki <mingwiki@gmail.com>
2022-03-23 16:12:34 +0800

    remove convert use Default Reveal.js

--- js/main.js
+++ js/main.js
@@ -1,69 +1,69 @@
 const isMain = str => (/^#{1,2}(?!#)/).test(str)
 const isSub = str => (/^#{3}(?!#)/).test(str)
-const convert = raw => {
-  let arr = raw.split(/\n(?=\s*#)/).filter(s => s != "").map(s => s.trim())
-  let html = ''
-  for (let i = 0; i < arr.length; i++) {
+// const convert = raw => {
+//   let arr = raw.split(/\n(?=\s*#)/).filter(s => s != "").map(s => s.trim())
+//   let html = ''
+//   for (let i = 0; i < arr.length; i++) {
 
-    if (arr[i + 1] !== undefined) {
-      if (isMain(arr[i]) && isMain(arr[i + 1])) {
-        html += `
-<section data-markdown>
-<textarea data-template>
-${arr[i]}
-</textarea>
-</section>
-`
-      } else if (isMain(arr[i]) && isSub(arr[i + 1])) {
-        html += `
-<section>
-<section data-markdown>
-<textarea data-template>
-${arr[i]}
-</textarea>
-</section>
-`
-      } else if (isSub(arr[i]) && isSub(arr[i + 1])) {
-        html += `
-<section data-markdown>
-<textarea data-template>
-${arr[i]}
-</textarea>
-</section>
-`
-      } else if (isSub(arr[i]) && isMain(arr[i + 1])) {
-        html += `
-<section data-markdown>
-<textarea data-template>
-${arr[i]}
-</textarea>
-</section>
-</section>
-`
-      }
-    } else {
-      if (isMain(arr[i])) {
-        html += `
-<section data-markdown>
-<textarea data-template>
-${arr[i]}
-</textarea>
-</section>
-`
-      } else if (isSub(arr[i])) {
-        html += `
-<section data-markdown>
-<textarea data-template>
-${arr[i]}
-</textarea>
-</section>
-</section>
-`
-      }
-    }
-  }
-  return html;
-}
+//     if (arr[i + 1] !== undefined) {
+//       if (isMain(arr[i]) && isMain(arr[i + 1])) {
+//         html += `
+// <section data-markdown>
+// <textarea data-template>
+// ${arr[i]}
+// </textarea>
+// </section>
+// `
+//       } else if (isMain(arr[i]) && isSub(arr[i + 1])) {
+//         html += `
+// <section>
+// <section data-markdown>
+// <textarea data-template>
+// ${arr[i]}
+// </textarea>
+// </section>
+// `
+//       } else if (isSub(arr[i]) && isSub(arr[i + 1])) {
+//         html += `
+// <section data-markdown>
+// <textarea data-template>
+// ${arr[i]}
+// </textarea>
+// </section>
+// `
+//       } else if (isSub(arr[i]) && isMain(arr[i + 1])) {
+//         html += `
+// <section data-markdown>
+// <textarea data-template>
+// ${arr[i]}
+// </textarea>
+// </section>
+// </section>
+// `
+//       }
+//     } else {
+//       if (isMain(arr[i])) {
+//         html += `
+// <section data-markdown>
+// <textarea data-template>
+// ${arr[i]}
+// </textarea>
+// </section>
+// `
+//       } else if (isSub(arr[i])) {
+//         html += `
+// <section data-markdown>
+// <textarea data-template>
+// ${arr[i]}
+// </textarea>
+// </section>
+// </section>
+// `
+//       }
+//     }
+//   }
+//   return html;
+// }
 const $ = s => document.querySelector(s)
 const $$ = s => document.querySelectorAll(s)
 
@@ -133,7 +133,8 @@ const Revealjs = {
     })
   },
   load() {
-    $('.reveal .slides').innerHTML = convert(localStorage.markdown || `# Slides.md`)
+    // $('.reveal .slides').innerHTML = convert(localStorage.markdown || `# Slides.md`)
+    $('.reveal .slides textarea').innerHTML = localStorage.markdown || `# Slides.md`
   }
 }
 const App = {
